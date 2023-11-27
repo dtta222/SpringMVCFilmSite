dcb7391998514fff62ffc36a4d0be0f877798783
dtta222 <david.t.taylor222@gmail.com>
2023-11-27 07:11:27 -0500

    update project

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # SpringMVCFilmSite
+
+# Description:
+The SpringMVCFilmSite is a web application that allows users to interact with a film database. It provides features such as searching for films by index or description, retrieving detailed information about a specific film, and listing films based on a keyword. The application is built using the Spring MVC framework, which facilitates the development of a robust and scalable web application.
+
+# Technologies Used:
+Spring MVC: The core framework used for building the web application, providing a model-view-controller architecture.
+
+Java: The primary programming language for developing the backend logic of the application.
+
+JSP (JavaServer Pages): Used for creating dynamic web pages and rendering data on the frontend.
+
+JSTL (JavaServer Pages Standard Tag Library): Utilized for incorporating dynamic functionality into JSP pages.
+
+MySQL: The relational database management system used to store and retrieve film-related data.
+
+HTML and CSS: Used for structuring and styling the web pages to enhance the user interface.
+
+JUnit: Employed for unit testing to ensure the correctness of individual components.
+
+Git: Used for version control, allowing for collaborative development and code management.
+
+# Lessons Learned:
+Spring MVC Architecture: Understanding and implementing the model-view-controller architecture in a web application.
+
+Integration with a Database: Incorporating a relational database into a Spring MVC application for data storage and retrieval.
+
+Web Development with JSP: Creating dynamic web pages using JavaServer Pages and incorporating JSTL for improved functionality.
+
+Unit Testing with JUnit: Writing and executing unit tests to validate the correctness of individual components.
+
+Version Control with Git: Using Git for version control, enabling collaboration and code management within a team.
+
+HTML and CSS Skills: Applying HTML and CSS for structuring and styling web pages to enhance the user experience.
+
+Error Handling and Exception Management: Implementing strategies for handling errors and exceptions gracefully in a web application.
+
+Collaborative Development: Gaining experience in collaborative development, understanding code integration, and resolving conflicts in a team environment.
+
+Best Practices in Web Development: Following best practices in web development, including code organization, naming conventions, and code readability.
--- src/test/java/com/skilldistillery/film/data/FilmDAOJDBCImplTest.java
+++ src/test/java/com/skilldistillery/film/data/FilmDAOJDBCImplTest.java
@@ -2,39 +2,104 @@ package com.skilldistillery.film.data;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.List;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import com.skilldistillery.film.entities.Actor;
+import com.skilldistillery.film.entities.Category;
 import com.skilldistillery.film.entities.Film;
+import com.skilldistillery.film.entities.Language;
 
 class FilmDAOJDBCImplTest {
 
-	 private FilmDAOJDBCImpl dao;
+	private FilmDAOJDBCImpl dao;
 
-	 @BeforeEach
-	 void setUp() throws Exception {
-	 	dao = new FilmDAOJDBCImpl();
-	 }
+	@BeforeEach
+	void setUp() throws Exception {
+		dao = new FilmDAOJDBCImpl();
+	}
 
-	 @AfterEach
-	 void tearDown() throws Exception {
-	 	dao = null;
-	 }
+	@AfterEach
+	void tearDown() throws Exception {
+		dao = null;
+	}
 
-	 @Test
-	 void test_findById_returns_film() {
-	 	Film film = dao.findById(1);
-	 	assertNotNull(film);
-	 	//assertEquals("ACADEMY DINOSAUR", film.getTitle());
-	 }
-	 
-	 @Test
-	 void test_findById_returns_null_for_invalid_id() {
-	 	Film film = dao.findById(1234567);
-	 	assertNull(film);
-	 }
-	 
-	 
+	@Test
+	void test_findById_returns_film() {
+		Film film = dao.findById(1);
+		assertNotNull(film);
+		assertEquals("ACADEMY DINOSAUR", film.getTitle());
+	}
 
+	@Test
+	void test_findById_returns_null_for_invalid_id() {
+		Film film = dao.findById(1234567);
+		assertNull(film);
+	}
+
+	@Test
+	void test_searchByKeyword_returns_films() {
+		List<Film> films = dao.searchByKeyword("comedy");
+		assertNotNull(films);
+		assertFalse(films.isEmpty());
+	}
+
+	@Test
+	void test_searchByKeyword_returns_empty_list_for_invalid_keyword() {
+		List<Film> films = dao.searchByKeyword("invalidKeyword123");
+		assertNotNull(films);
+		assertTrue(films.isEmpty());
+	}
+
+	@Test
+	void test_findLanguageById_returns_language() {
+		Language language = dao.findLanguageById(1);
+		assertNotNull(language);
+		assertEquals("English", language.getName());
+	}
+
+	@Test
+	void test_findLanguageById_returns_null_for_invalid_language_id() {
+		Language language = dao.findLanguageById(1234567);
+		assertNull(language);
+	}
+
+	@Test
+	void test_findActorsByFilmId_returns_actors() {
+		List<Actor> actors = dao.findActorsByFilmId(1);
+		assertNotNull(actors);
+		assertFalse(actors.isEmpty());
+	}
+
+	@Test
+	void test_findActorsByFilmId_returns_empty_list_for_invalid_film_id() {
+		List<Actor> actors = dao.findActorsByFilmId(1234567);
+		assertNotNull(actors);
+		assertTrue(actors.isEmpty());
+	}
+	
+	@Test
+	void test_category_constructor_and_getters() {
+		int id = 1;
+		String categoryName = "Action";
+
+		Category category = new Category(id, categoryName);
+
+		assertEquals(id, category.getId());
+		assertEquals(categoryName, category.getCategory());
+	}
+
+	@Test
+	void test_category_equals() {
+		Category category1 = new Category(1, "Action");
+		Category category2 = new Category(1, "Action");
+		Category category3 = new Category(7, "Drama");
+
+		assertTrue(category1.equals(category2));
+		assertFalse(category1.equals(category3));
+		assertFalse(category2.equals(category3));
+	}
 }
